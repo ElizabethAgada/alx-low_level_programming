ca04cf34f2363f8a9fa4cf3170a7ab35c8d56d70
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-14 11:57:03 -0700

    variables if, else and while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,10 +7,12 @@
 int main(void)
 
 /**
-* main - using the if and else int;
+* main - main function
 *
 * Return: always 0
+*/
 
+int main(void)
 {
 	int n;
 
@@ -19,7 +21,7 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 		printf("n%d is positive", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("n%d is zero", n);
 	else if (n < 0)
 		printf("n%d is negative", n);
