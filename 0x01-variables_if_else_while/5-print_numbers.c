a0b6ed12e825a72a58f6df862296800b4b57772d
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-15 06:25:15 -0700

    print number zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:  print numbers from zero to nine
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
