0dffde9e1dcbc0d37b5f4ed914354e7e47433614
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-14 21:59:01 -0700

    print alphabets but exclude some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main -Entry point
+ * Description: 'print letters and exclude some'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+		putcher(i);
+		putcher('\n')
+	return (0);
+}
