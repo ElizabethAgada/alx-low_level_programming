a74c5722932ff341361fe5559439dd5a506b6ad0
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-15 05:51:31 -0700

    print alphabets but exclude some

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  * Description: 'print letters and exclude some'
  * Return: Always 0
  */
@@ -9,9 +9,11 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a'; i <= 'z'; i++)
+	{
 		if (i != 'q' && i != 'e')
 		putchar(i);
 		putchar('\n');
+	}
 	return (0);
 }
