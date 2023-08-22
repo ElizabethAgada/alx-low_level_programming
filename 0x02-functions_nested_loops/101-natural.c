ed0b8fe7fbf89b2c34ac5ee3dab6af69130f2953
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-22 01:48:49 -0700

    print 3 and 5 multiples in 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints multiple of 3 and 5 below 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
