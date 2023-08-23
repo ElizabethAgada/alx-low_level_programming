755d8b49d9fdc9300e4199b40771dc4b99672b54
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-23 05:05:56 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,10 @@ void print_triangle(int size)
 	else
 	{
 		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
