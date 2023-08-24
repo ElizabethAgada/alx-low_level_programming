bc4e4af1d496699d633a7ab97a1baa83320888ad
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-08-24 08:27:00 -0700

    return length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,10 +7,12 @@
  */
 int _strlen(char *s)
 {
-	int i;
-	int count = 0;
+	int longi = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-			count++;
-	return (count);
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
 }
