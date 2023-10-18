22eb819742e97ecc1b4e97e4580a97b6f8c4477b
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-10-18 05:36:24 -0700

    tasks 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_unit - converts binary nos to integers/decimal nos
- * @b: a char pointer to a string containing the binary nos
- * Return: the converted decimal (output)
+ * binary_to_uint - binary can be converted through unsigned int
+ * @b: A string that takes binary number or numbers
+ * Return: A numbers that are coverted
  */
-unsigned int binary_to_unit(const char *b)
+
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
-	size_t a = 0;
-	size_t c = 0;
-	size_t sum = 0;
-	size_t pow = 1;
-	int base4 = 2;
+	int len;
+	unsigned int grace = 0;
 
-	if (b == (NULL)
+	if (!b)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++)
-
-	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-		return (b[0] - 48);
-
-	for (a = 0; b[a] != '\0'; '1')
+	for (len = 0; b[len]; len++)
 	{
-		if (b[a] != '0' && b[a] != '1')
-		return (0);
-		for (c = len - 1; c > 0; c--)
-		pow = pow * base;
-		sum = sum + (power * (b[a] - 48));
-		len--;
-		pow = 1;
+		if (b[len] < '0' || b[len] > '1')
+			return (0);
+		grace = 2 * grace + (b[len] - '0');
 	}
-	return (sum);
+	return (grace);
 }
