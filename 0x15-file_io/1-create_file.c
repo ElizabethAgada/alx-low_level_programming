dfb70d880ee3292986aa39e0abbad250e4c8fb43
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-10-18 15:32:13 -0700

    task 1 create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - the cresating of file
+ * @filename: it points to the name of file to be created
+ * @text_content: it points to the string to write to the file
+ * Return: if function fails --1 otherwise - 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int love, peace, joy = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (joy = 0; text_content[joy];)
+			joy++;
+	}
+	love = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	peace = write(joy, text_content, joy);
+
+	if (love == -1 || peace == -1)
+		return (-1);
+
+	close(love);
+
+	return (1);
+}
