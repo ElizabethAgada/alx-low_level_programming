88e790239eec9e50fee14512648cf9639431de79
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-10-19 15:33:35 -0700

    task 4 cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,8 +6,9 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * creates_buffer - Allocates 1024 bytes for a buffer
- * @file: the name of the file buffer is storing chars for
+ * creates_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
  * Return: A pointer to the newly-allocated buffer.
  */
 char *creates_buffer(char *file)
@@ -27,8 +28,8 @@ char *creates_buffer(char *file)
 }
 
 /**
- * close_file - closes file descriptors
- * @fd: the file descriptor to be closed
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
 void close_file(int fd)
 {
@@ -38,16 +39,17 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can 't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - this copies the contents of a file to another file
- * @argc: the no of arguements to the program
- * @argv: an array of pointers to the arguements
- * Return: 0 on success
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
