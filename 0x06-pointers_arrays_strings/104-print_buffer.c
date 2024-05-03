a02091ebc48ac64eb34ba3633016afcbb08ebc96
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-05-03 12:32:13 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,49 +1,58 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_line - printssfsdfs a ssdffdss bysdfsdtes
+ * @c: buffer tosdfsdfsdfprint
+ * @s: bytes of buffer to print
+ * @l: line of buffer ssdto print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+int j, k;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+}
+for (k = 0; k <= s; k++)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
+putchar('.');
+}
+}
 
 /**
- * print_buffer - a function that prints a buffer
- * @b: buffer
- * @size: size
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int a, m, f;
-
-	a = 0;
-
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (a < size)
-	{
-		m = size - a < 10 ? size - a : 10;
-		printf("%08x: ", a);
-		for (f = 0; f < 10; f++)
-		{
-			if (f < m)
-				printf("%02x", *(b + a + f));
-			else
-				printf(" ");
-			if (f % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (f = 0; f < m; f++)
-		{
-			int c = *(b + a + f);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
-		printf("\n");
-		a += 10;
-	}
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
