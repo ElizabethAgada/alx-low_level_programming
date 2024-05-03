9a3bc888a7b0ba7a70ee45775d5524695d8b9105
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-05-04 00:19:05 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,11 +17,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL},
 	};
-
 	int p;
 	
 	p = 0;
-
 	while (ops[p].op != NULL && *(ops[p].op) != *s)
 		p++;
 	return (ops[p].f);
