4c2894c8ba24c3690d151b1bd9b44d6f1a0bd8ee
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-05-04 19:08:12 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Compile each .c file into an object file
-for file in *.c; do
-    if [ -f "$file" ]; then
-        gcc -c -fPIC "$file" -o "${file%.c}.o"
-    fi
-done
-
-# Create the dynamic library from the object files
+gcc -c -pedantic -Wall -Werror -Wextra -fPIC -c *.c
 gcc -shared -o liball.so *.o
-
-# Clean up the temporary object files
-rm *.o
