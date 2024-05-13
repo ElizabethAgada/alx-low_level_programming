f7a2c371263d59b6e2f6f7e1e2bc03b0fa8e676d
ElizabethAgada <140746363+ElizabethAgada@users.noreply.github.com>
2024-05-13 12:13:05 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -c -pedantic -Wall -Werror -Wextra -fPIC -c *.c
+
+# Compile all .c files in the current directory
+gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
+
+# Create the shared library
 gcc -shared -o liball.so *.o
+
+# Clean up - remove the object files
+rm *.o
